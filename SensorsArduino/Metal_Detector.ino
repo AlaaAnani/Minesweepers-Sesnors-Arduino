a3a2bf715278756e6e6664bab8604576ed72e0e0
Alaa Anani <30525304+AlaaAnani@users.noreply.github.com>
2018-08-20 03:05:41 -0700

    Add files via upload

---
+++ SensorsArduino/Metal_Detector.ino
@@ -0,0 +1,39 @@
+void MetalDetector()
+{
+  int f = digitalRead(MetalDecetor_pin);
+  RelayTimer = millis();
+ if(RelayTimer - LastTimer < 1000)
+ {
+  digitalWrite(MMRelay_pin, HIGH);
+  if(f)
+   { 
+    MineR = 1;
+    digitalWrite(Sound_pin, HIGH);
+     }
+  else
+    {MineR = 0;
+     digitalWrite(Sound_pin, LOW);
+    }
+ }
+  
+
+   
+  if(RelayTimer - LastTimer >= 1000)
+  {
+   digitalWrite(MMRelay_pin, LOW);
+   if(f)
+    {  MineL = 1;
+       digitalWrite(Sound_pin, HIGH);
+    
+    }
+    else{
+      MineL = 0;
+       digitalWrite(Sound_pin, LOW);
+    }
+      
+      if(RelayTimer - LastTimer >= 2000)
+         LastTimer = RelayTimer;
+  }
+
+}
+
